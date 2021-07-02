52c7c5966f657f55f27cdb5b3a59a95c91ae68f2
prathamoli <74040027+prathamoli@users.noreply.github.com>
2021-07-02 11:32:15 +0545

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,6 +7,11 @@ var cors = require('cors')
 
 //adding cors 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 router.get('/',(req, res) =>{
     res.json(data)
 })
