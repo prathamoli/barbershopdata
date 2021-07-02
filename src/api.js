b90b06257f528ace3aebbaade5a9d8190ea026dd
prathamoli <74040027+prathamoli@users.noreply.github.com>
2021-07-02 11:49:26 +0545

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,10 +3,8 @@ const serverless = require('serverless-http')
 const data = require("../category")
 const app = express()
 const router = express.Router()
-var cors = require('cors')
 
-//adding cors 
-app.use(cors())
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
